428e4448003d0aa130112e7a03bb9b7b9ed254b9
Leonardo Rebouças Batista <reboucas.beraby@gmail.com>
2022-05-26 22:46:43 -0300

    login page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,16 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import BasicLayout from "layout/Basic";
+import React from "react";
+import { useRouter } from "next/router";
 
 const Home: NextPage = () => {
+  const router = useRouter();
+
+  React.useEffect(() => {
+    router.push("/auth/sigin");
+  }, []);
+
   return (
     <BasicLayout>
       <Head>
