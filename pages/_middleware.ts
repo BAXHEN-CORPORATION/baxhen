4b8996960910af42f2c586182bf926259c89273f
Leonardo Rebouças Batista <reboucas.beraby@gmail.com>
2022-09-16 12:00:47 -0300

    chore: tryc catch

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -4,21 +4,25 @@ import { getSerializedLocale } from "utils";
 const PUBLIC_FILE = /\.(.*)$/;
 
 export function middleware(request: NextRequest) {
-  const userLocale =
-    request.headers.get("accept-language")?.split(",")[0] || "";
-  const locale = getSerializedLocale(userLocale);
+  try {
+    const userLocale =
+      request.headers.get("accept-language")?.split(",")[0] || "";
+    const locale = getSerializedLocale(userLocale);
 
-  const shouldHandleLocale =
-    !PUBLIC_FILE.test(request.nextUrl.pathname) &&
-    !request.nextUrl.pathname.includes("/api/") &&
-    request.nextUrl.locale === "default";
+    const shouldHandleLocale =
+      !PUBLIC_FILE.test(request.nextUrl.pathname) &&
+      !request.nextUrl.pathname.includes("/api/") &&
+      request.nextUrl.locale === "default";
 
-  if (shouldHandleLocale) {
-    const url = request.nextUrl.clone();
+    if (shouldHandleLocale) {
+      const url = request.nextUrl.clone();
 
-    url.locale = locale;
+      url.locale = locale;
 
-    return NextResponse.redirect(url);
+      return NextResponse.redirect(url);
+    }
+  } catch (e) {
+    console.log({ e });
   }
 
   return undefined;
